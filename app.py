bd7fa29a510b948dcb100bf1d4088490ba0e9d55
Infamous003 <arfathsyed0312@gmail.com>
2025-05-23 14:36:29 +0530

    Add dependency injection for sessions and lifespan event to initialize database on startup

--- app.py
+++ app.py
@@ -1,71 +1,84 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from models import PostCreate, Post, PostUpdate
-from database import engine, init_db
+from database import engine, init_db, get_sesssion
 from sqlmodel import select, Session
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Blog API", description="This is a simple blog api")
 
-init_db()
+# This piece of code will make sure that the db is created before we start making requests
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+
+
+app = FastAPI(
+    title="Blog API",
+    description="This is a simple blog api",
+    lifespan=lifespan)
+
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
 
 @app.get("/posts")
-def get_posts():
-    with Session(engine) as session:
-        query = select(Post)
-        posts = session.exec(query).fetchall()
+def get_posts(session: Session = Depends(get_sesssion)):
+    
+    query = select(Post)
+    posts = session.exec(query).fetchall()
 
-        if not posts:
-            return {'message': 'Looks so empty...'}
-        return posts
+    if not posts:
+        return {'message': 'Looks so empty...'}
+    return posts
+
 
 @app.get("/posts/{id}")
-def get_posts(id: int):
-    with Session(engine) as session:
-        query = select(Post).where(Post.id == id)
-        post_found = session.exec(query).one_or_none()
-        if post_found is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Post not found')
-        return post_found
+def get_posts(id: int, session: Session = Depends(get_sesssion)):
+    query = select(Post).where(Post.id == id)
+    post_found = session.exec(query).one_or_none()
+    if post_found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Post not found')
+    return post_found
+
 
 @app.post("/posts")
-def create_posts(post: PostCreate):
-    with Session(engine) as session:
-        new_post = Post(**post.model_dump())
-        session.add(new_post)
-        session.commit()
-        session.refresh(new_post)
-        return new_post
+def create_posts(post: PostCreate, session: Session = Depends(get_sesssion)):
+    # with Session(engine) as session:
+    new_post = Post(**post.model_dump())
+    session.add(new_post)
+    session.commit()
+    session.refresh(new_post)
+    return new_post
+
 
 @app.put("/posts/{id}")
-def update_posts(id: int, post: PostUpdate):
+def update_posts(id: int, post: PostUpdate, session: Session = Depends(get_sesssion)):
+    query = select(Post).where(Post.id == id)
+    post_found = session.exec(query).one_or_none()
+
+    if post_found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     
-    with Session(engine) as session:
-        query = select(Post).where(Post.id == id)
-        post_found = session.exec(query).one_or_none()
+    if post.title: post_found.title = post.title
+    if post.description: post_found.description = post.description
 
-        if post_found is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-        
-        if post.title: post_found.title = post.title
-        if post.description: post_found.description = post.description
-
-        session.add(post_found)
-        session.commit()
-        session.refresh(post_found)
-        return post_found
+    session.add(post_found)
+    session.commit()
+    session.refresh(post_found)
+    return post_found
 
 @app.delete("/posts/{id}")
-def delete_posts(id: int):
-    with Session(engine) as session:
-        query = select(Post).where(Post.id == id)
-        post_found = session.exec(query).one_or_none()
+def delete_posts(id: int, session: Session = Depends(get_sesssion)):
 
-        if post_found is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    
-        session.delete(post_found)
-        session.commit()
-        return {'message': 'Post successfully deleted'}
+    query = select(Post).where(Post.id == id)
+    post_found = session.exec(query).one_or_none()
+
+    if post_found is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+    session.delete(post_found)
+    session.commit()
+    return {'message': 'Post successfully deleted'}
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
 # DATABASE_URL = "postgresql://username:password@hostname:port/database_name"
 
 DATABASE_URL = "postgresql://postgres:password@localhost:5432/fastapi"
@@ -7,3 +7,9 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+# This function enables us to use Depends and not repeat code by creating
+# a session again and agin for each db access
+def get_sesssion():
+    with Session(engine) as session:
+        yield session
