bb74f788e4cdd80180a64b7756f8eeecd03c83de
Infamous003 <arfathsyed0312@gmail.com>
2025-05-26 15:18:08 +0530

    Add relationship between posts and users

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
 class Post(SQLModel, table=True):
     __tablename__ = "posts"
@@ -7,14 +7,19 @@ class Post(SQLModel, table=True):
     title: str = Field(nullable=False, max_length=64)
     description: str = Field(nullable=False, max_length=1024)
 
+    user_id: int = Field(foreign_key="users.id")
+    user: list["User"] = Relationship(back_populates="post")
+
 class PostPublic(BaseModel):
     id: int
     title: str
     description: str
+    user_id: int
 
 class PostCreate(BaseModel):
     title: str
     description: str
+    user_id: int
 
 class PostUpdate(BaseModel):
     title: str | None = None
@@ -27,6 +32,8 @@ class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(max_length=16, nullable=False, unique=True)
     password: str = Field(max_length=64, min_length=4, nullable=False, unique=False)
+
+    post: Post = Relationship(back_populates="user") # the value of back_populates is the relationship name, not table name
 
 class UserCreate(BaseModel):
     username: str
